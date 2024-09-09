f8e698527f50af4bf7d3796351f41b6b1b79af42
artmigalev <art_migalev@yahoo.com>
2024-09-09 21:50:10 +0300

    "init: start cssMemeSlider-task `LANG=en_EN date +'(%a, %b %d, %Y %r)'`"

---
+++ cssMemeSlider/index.js
@@ -0,0 +1,9 @@
+const slides = document.querySelector('.slides')
+const pagination = document.querySelector(".pagination");
+
+const startIndex = 0;
+
+pagination.addEventListener('click', handle);
+
+
+
